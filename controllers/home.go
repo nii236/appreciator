df8f65a91cb31a218fdbab18abd69257e1767aab
John Nguyen <jtnguyen236@gmail.com>
2016-06-03 10:51:41 +0800

    added cutestrap, basic template, render html

--- controllers/home.go
+++ controllers/home.go
@@ -14,6 +14,6 @@ type HomeController struct {
 
 // Index contains the index action for the home controller
 func (c *HomeController) Index(w http.ResponseWriter, r *http.Request) error {
-	c.JSON(w, 200, map[string]string{"derp": "sherp"})
+	c.HTML(w, http.StatusOK, "example", "world")
 	return nil
 }
--- web/web.go
+++ web/web.go
@@ -15,6 +15,7 @@ func Run() {
 	c := &controllers.HomeController{Render: render.New(render.Options{})}
 	r := mux.NewRouter()
 	r.Handle("/", c.Action(c.Index)).Methods("GET")
+	r.PathPrefix("/").Handler(http.FileServer(http.Dir("./public/")))
 	err := http.ListenAndServe(":8080", r)
 	if err != nil {
 		log.Fatalln("Error!", err)
