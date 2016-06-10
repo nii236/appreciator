14e1b1725bcec3f0f0d073f10e852d7ea5244a51
John Nguyen <jtnguyen236@gmail.com>
2016-06-10 14:32:10 +0800

    add new routes for rsvp endpoints

--- models/db.go
+++ models/db.go
@@ -5,8 +5,14 @@ import (
 	"github.com/siddontang/ledisdb/ledis"
 )
 
+// DB Contains an instance of the DB
 type DB struct {
 	*ledis.DB
+}
+
+// DBer contains all the interfaces from the models
+type DBer interface {
+	Guester
 }
 
 // NewDB creates a new instance of a database
--- web/web.go
+++ web/web.go
@@ -18,19 +18,25 @@ import (
 func Run() {
 	adminHome := &admin.HomeController{Render: amber.New()}
 	memberHome := &member.HomeController{Render: amber.New()}
+	memberRSVP := &member.RSVPController{Render: amber.New()}
 
 	r := httprouter.New()
 	r.ServeFiles("/public/*filepath", http.Dir("public/"))
 
 	r.GET("/admin", adminHome.Action(adminHome.Index))
-
 	r.GET("/member", memberHome.Action(memberHome.Index))
 	r.POST("/member", memberHome.Action(memberHome.Create))
+
+	r.GET("/rsvp", memberRSVP.Action(memberRSVP.Index))
+	r.GET("/rsvp/do", memberRSVP.Action(memberRSVP.RSVP))
+	r.POST("/rsvp/complete", memberRSVP.Action(memberRSVP.Complete))
+	r.POST("/rsvp", memberRSVP.Action(memberRSVP.Login))
 
 	n := negroni.New()
 	n.Use(negroni.HandlerFunc(loggerMiddleware))
 	n.Use(negroni.NewRecovery())
 	n.UseHandler(r)
+
 	PORT := os.Getenv("PORT")
 	if PORT == "" {
 		PORT = ":3000"
